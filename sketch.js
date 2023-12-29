637eada634e9b915216ec7e8756431ddb801c6cb
cxcuser <ibyam360@gmail.com>
2023-12-29 09:51:50 -0400

    Remove some superfluous code.
    Remove some variables and functions that were 'experimental', and retain (mainly)
    just what is actually needed or being used.

--- sketch.js
+++ sketch.js
@@ -1,44 +1,69 @@
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
-
-const btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click',  (e) => {
-  alert("Hello World");
-  console.log("Hello E", e.target);
-});
 
 const maindiv = document.querySelector('.container');
-const gnum = document.querySelector('#gnum');
-const gbtn = document.querySelector('#gbtn');
-const delbtn = document.querySelector('#delbtn');
+
+const selbtn =document.querySelector("#selbtn");
+const numsqbtn =document.querySelector("#numsqbtn");
+const gridarea = document.querySelector("#gridarea");
+const numsquares = document.querySelector("#numsquares");
+const newgrid = document.querySelector("#newgrid");
 
 const gFrag = document.createDocumentFragment();
 
-gnum.addEventListener('keydown', pressFrag);
-/*gnum.addEventListener('keydown', pressGrid);*/
-gbtn.addEventListener('click', clickGrid);
-delbtn.addEventListener('click', delFrag);
+let num;  /*value from no. of boxes selector */
+let numsq;  /*actual number of boxes - num*num */
+let boxsize;  /* assign to parseInt(gridarea.value)/parseInt(numsquares.value) */
+let colornow;
+let boxmessage = document.querySelector("#boxmessage");
 
-function pressFrag(e) {     /*Similar to pressGrid, but using a fragment*/
-  if (e.key == "Enter") {   /* instead of adding each div to the DOM individually */
-    e.preventDefault();
+selbtn.addEventListener('click', getGsize); /* doesn't seem to be called from selbtn */
 
-    for (let i=0; i<gnum.value; i++) {
+newgrid.addEventListener('click', newFrag);
+
+/*
+Use associative array/object matching options with no. of boxes to find
+no. of boxes to create in the grid area. 
+*/
+
+function getGsize() {
+    maindiv.style["width"] = (parseInt(gridarea.value) + 4) + "px";
+    maindiv.style["height"] = (parseInt(gridarea.value) + 4) + "px";
+    console.log(num, "numsquares value");
+}  /*Use +4 here to avoid having the border pixels interfere with
+     box sizes at the borders, I think */
+
+function newFrag() {     /*Similar to pressFrag, but using a button*/
+                        /* instead of a key press on an input field.*/
+   
+    num = parseInt(numsquares.value);
+    numsq = num*num;  /* number of new divs to create for the gridarea */
+    delFrag();  /* Delete previous grid from maindiv */
+
+    getGsize(); /* Take size input sand draw new grid outline. */
+
+    for (let i=0; i<numsq; i++) {
       let newdiv = document.createElement('div');
       newdiv.classList.add('gbox');
       /*newdiv.style["background-color"] = "pink";*/
-      newdiv.style["height"] = "200px";
+      boxsize =  parseInt(gridarea.value)/num;
+      newdiv.style["height"] = boxsize+"px";
+      newdiv.style["width"] = boxsize +"px";
 
+      colornow = newdiv.style["background-color"];
       newdiv.addEventListener('mouseover', sizeChange);
       newdiv.addEventListener('mouseout', sizeRestore);
+
+      newdiv.addEventListener('mouseover', colorChange);
+      newdiv.addEventListener('mouseout', colorRestore);
 
       gFrag.appendChild(newdiv);
 
     } 
-    maindiv.appendChild(gFrag);
-    gnum.value = '';
-    gnum.focus();    
-  }
+    maindiv.appendChild(gFrag);  /*All new divs appended to newFrag; 
+                                  now append newfrag to maindiv. */
+    /*  *** FIX THIS   ** */
+    boxsize = Math.floor(boxsize);
+    boxmessage.textContent = "Each box in the grid is now " +boxsize +"px by " + boxsize +"px.";
+
 }
 
 function delFrag() {     
@@ -48,42 +73,25 @@ function delFrag() {
       /* maindiv.innerHTML = ''; */
       maindiv.textContent = '';
     }
-    gnum.value = '';
-    gnum.focus();    
-}
-
-function pressGrid(e) {
-  if (e.key == "Enter") {
-    e.preventDefault();
-
-    for (let i=0; i<gnum.value; i++) {
-      let newdiv = document.createElement('div');
-      newdiv.classList.add('gbox');
       
-      maindiv.appendChild(newdiv);
-
-    } /*Change to use fragment instead*/
-    gnum.value = '';
-    gnum.focus();    
-  }
 }
+
 
 function sizeChange(e) {
-  e.target.style["width"] = "120px";
+  e.target.style["width"] = e.target.style["width"];
 }
 
 function sizeRestore(e) {
-  e.target.style["width"] = "100px";
+  
+  e.target.style["width"] = e.target.style["width"]
+}
+function colorChange(e) {
+  e.target.style["background-color"] = "#faa";
 }
 
-function clickGrid() {
-      for (let i=0; i<gnum.value; i++) {
-      let newdiv = document.createElement('div');
-      newdiv.classList.add('gbox');
-      maindiv.appendChild(newdiv);
-    } /*Change to use fragment instead*/
-    gnum.value = '';
-    gnum.focus();    
+function colorRestore(e) {
+  
+  e.target.style["background-color"] = colornow; /*color from css file*/
 }
 
 
@@ -91,58 +99,8 @@ function clickGrid() {
 
 
 
-const ulvar = document.querySelector('ul');
 
-const addme = document.querySelector('#addme');
-const inpval = document.querySelector('#item');
-const myform = document.querySelector('#myform');
 
-function addli(e) {
-  /*const newTextNode = document.createElement.text*/
-  
-   let newli = document.createElement('li');
-   let newspan = document.createElement('span');
-   let newbtn = document.createElement('button');
-   newbtn.textContent = "Delete";
-   newbtn.addEventListener('click', () => {
-    /*alert("DELETE PARENT");*/
-    newbtn.parentElement.remove();
-   })
-   newspan.textContent = inpval.value + " ";
-   newbtn.classList.add("listbtn");
-   inpval.value ='';
-   inpval.focus();
-   
-   newli.appendChild(newspan);
-   newli.appendChild(newbtn);
-   ulvar.appendChild(newli);
-   
-  /*alert("ADD ME"); */
-};
 
-addme.addEventListener('click', addli);
-inpval.addEventListener('keydown', addLiByKey);
 
-function addLiByKey(e) {
-  
-  if (e.key == "Enter") {
-    e.preventDefault();
-    let newli = document.createElement('li');
-    let newspan = document.createElement('span');
-    let newbtn = document.createElement('button');
-    newbtn.textContent = "Delete";
-    newbtn.addEventListener('click', () => {
-      /*alert("DELETE PARENT");*/
-      newbtn.parentElement.remove();
-    })
-    newspan.textContent = inpval.value + " ";
-    newbtn.classList.add("listbtn");
-    inpval.value ='';
-    inpval.focus();
-    
-    newli.appendChild(newspan);
-    newli.appendChild(newbtn);
-    ulvar.appendChild(newli);
-    
-  }
-};
+
